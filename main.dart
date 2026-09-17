1ce3f258c1eff112a1bc314a12f53024c992c092
azizasabit-ui <aziza.sabi@narxoz.kz>
2026-09-17 22:19:02 +0500

    Create library class

--- main.dart
+++ main.dart
@@ -11,3 +11,20 @@ class Book {
     this.isBorrowed = false,
   });
 }
+class Library {
+  final List<Book> _books = [];
+
+  void addBook(Book book) {
+    _books.add(book);
+  }
+
+  List<Book> getAvailableBooks() {
+    return _books.where((book) => !book.isBorrowed).toList();
+  }
+  double getTotalValue() {
+    return _books.fold(
+      0.0,
+      (total, book) => total + book.price,
+    );
+  }
+}
