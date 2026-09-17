447f11133688bb95aaa4acaec5cfbe008e268680
azizasabit-ui <aziza.sabi@narxoz.kz>
2026-09-17 22:14:52 +0500

    Create book class with properties

---
+++ main.dart
@@ -0,0 +1,13 @@
+class Book {
+  String title;
+  String author;
+  double price;
+  bool isBorrowed;
+
+  Book({
+    required this.title,
+    required this.author,
+    required this.price,
+    this.isBorrowed = false,
+  });
+}
